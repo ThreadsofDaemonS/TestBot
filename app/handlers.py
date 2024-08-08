4eef1747981103a86975d38bc467ba1ded84f154
W Q <vbnzuf@gmail.com>
2024-08-08 22:50:50 +0300

    Was added inline buttons

--- app/handlers.py
+++ app/handlers.py
@@ -1,7 +1,7 @@
 
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 
 import app.keyboards as kb
@@ -10,7 +10,8 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(msg: Message):
-    await msg.reply(f"Привет!\n твой id: {msg.from_user.id} Имя: {msg.from_user.first_name}", reply_markup=kb.main)
+    await msg.reply(f"Привет!\n твой id: {msg.from_user.id} Имя: {msg.from_user.first_name}",
+                    reply_markup=kb.main)
 
 @router.message(Command("help"))
 async def get_help(msg: Message):
@@ -19,3 +20,8 @@ async def get_help(msg: Message):
 @router.message(F.text == "Как дела?")
 async def whats_up(msg: Message):
     await msg.answer("ОК")
+
+@router.callback_query(F.data == "catalog")
+async def catalog(callback: CallbackQuery):
+    await callback.answer('Вы выбрали каталог', show_alert=True)
+    await callback.message.edit_text("Вот наш каталог!", reply_markup=await kb.inline_cars())
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,7 +1,29 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="Каталог")],
-    [KeyboardButton(text="Корзина"), KeyboardButton(text="Контакты")]
-], resize_keyboard=True,
-    input_field_placeholder="Выберете пункт меню.")
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+# main = ReplyKeyboardMarkup(keyboard=[
+#     [KeyboardButton(text="Каталог")],
+#     [KeyboardButton(text="Корзина"), KeyboardButton(text="Контакты")]
+# ], resize_keyboard=True,
+#     input_field_placeholder="Выберете пункт меню.")
+
+main = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Каталог", callback_data='catalog')],
+    [InlineKeyboardButton(text="Корзина", callback_data='trash'), InlineKeyboardButton(text="Контакты",
+                                                                                       callback_data='contacts')]
+])
+
+
+settings = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Youtube", url='https://www.youtube.com/@ShadowStormlq5mwdasd')]
+])
+
+
+cars = ["Tesla", "BMW","Audi", "Mersedes"]
+
+async def inline_cars():
+    keyboard = InlineKeyboardBuilder()
+    for car in cars:
+        keyboard.add(InlineKeyboardButton(text=car, url="https://google.com"))
+    return keyboard.adjust(2).as_markup()
